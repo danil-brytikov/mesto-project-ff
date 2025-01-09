8a9844a36c5093eb5d12b58afa7ac4e6785c3d6f
Danil Brytikov <danilbrytikov@yandex.ru>
2025-01-09 14:30:21 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list'); 
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки  
+
+function addCard (cardContent) {
+    const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    card.querySelector('.card__image').src = cardContent.link;
+    card.querySelector('.card__image').setAttribute("alt", `Изображение ${cardContent.name}`);
+    card.querySelector('.card__title').textContent = cardContent.name;
+
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', deleteCard);
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard() {
+    this.parentElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(cardContent => placesList.append(addCard(cardContent)));
